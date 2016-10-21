8edb762a0354ce6b4135db774dd1ed2a4adde0c6
Rohan Sharma <racy.rohan@gmail.com>
2016-10-21 10:07:02 +0530

    Added Splash Activity

--- app/src/main/java/com/fancypackagename/rohansharma/closet/SplashActivity.java
+++ app/src/main/java/com/fancypackagename/rohansharma/closet/SplashActivity.java
@@ -2,6 +2,10 @@ package com.fancypackagename.rohansharma.closet;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_splash);
+
+        YoYo.with(Techniques.FadeIn)
+                .duration(500)
+                .interpolate(new AccelerateDecelerateInterpolator())
+                .playOn(findViewById(R.id.imageView));
     }
 }
